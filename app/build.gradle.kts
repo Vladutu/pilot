5ed45b15c6fb539ae4f05c153d00e1424520066d
Georgian Vladutu <georgian.vladutu-ext@power.cloud>
2026-06-01 17:03:53 +0300

    test(pilot): enable returnDefaultValues so android.util.Log no-ops in unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,9 @@ android {
     packaging {
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
 }
 
 dependencies {
